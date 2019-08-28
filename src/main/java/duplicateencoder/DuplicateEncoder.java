d9fd36a896f10518825b7cb47a4e1627cbdcc506
Piotr Kopalko <copalco@gmail.com>
2019-08-28 14:35:57 +0200

    Break lines.

--- src/main/java/duplicateencoder/DuplicateEncoder.java
+++ src/main/java/duplicateencoder/DuplicateEncoder.java
@@ -4,7 +4,9 @@ public class DuplicateEncoder {
 
     public static String encode(String text) {
         Text normalizedText = new Text(text);
-        return normalizedText.characterStream().map(c -> encode(c, normalizedText)).reduce("", String::concat);
+        return normalizedText.characterStream()
+                .map(c -> encode(c, normalizedText))
+                .reduce("", String::concat);
     }
 
     private static String encode(String character, Text normalizedText) {
